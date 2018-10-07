d80b21c24e2ddfe8d0017c471dbaa834ed74d4f4
David Honkys <david.honkys@tieto.com>
2018-10-07 11:30:29 +0200

    Basics of output

---
+++ src/j4b/lab2/io/BasicOutput.java
@@ -0,0 +1,19 @@
+package j4b.lab2.io;
+
+/**
+ * 
+ * Basic difference between "print" and "println" 
+ *
+ */
+public class BasicOutput {
+	
+	public static void main(String[] args) {
+	    	
+	    	System.out.println("1. println ");
+	    	System.out.println("2. println ");
+	    	
+	    	System.out.print("1. print ");
+	    	System.out.print("2. print");
+	    }
+	
+}
